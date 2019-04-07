815a51921ae40dcf457874b47a8500e401af0903
Naoki Kishi <naokikishi.kyoto@gmail.com>
2019-04-07 17:20:46 +0900

    add vscode setting

---
+++ vscode/setup_mac.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+ln -s $HOME/dotfiles/vscode/User/ ~/Library/Application\ Support/Code/User
+ln -s $HOME/dotfiles/vscode/extensions/ ~/.vscode/extensions
---
+++ vscode/setup_ubuntu.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+ln -s $HOME/dotfiles/vscode/User/ $HOME/.config/Code/User
+ln -s $HOME/dotfiles/vscode/extensions/ $HOME/.vscode/extensions
