f669fadb61e5102f60db1f78c204a242905199ec
Naoki Kishi <naokikishi.kyoto@gmail.com>
2020-07-30 18:13:29 +0900

    setup zsh

--- setup.sh
+++ setup.sh
@@ -11,3 +11,11 @@ do
     unlink $HOME/$f
     ln -s $(cd $(dirname $0); pwd)/$f $HOME/$f
 done
+
+chsh -s `which zsh`
+git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
+
+setopt EXTENDED_GLOB
+for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
+  ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
+done
