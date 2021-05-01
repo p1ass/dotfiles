1e0e6196fe1c21b5095fefee239b5768b9469eba
Naoki Kishi <naokikishi.kyoto@gmail.com>
2021-05-01 14:53:16 +0900

    remove vscode setting

--- backup_vscode.sh
+++
@@ -1,3 +0,0 @@
-#! /bin/bash
-
-code --list-extensions > vscode-extensions.txt
--- restore_vscode.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-
-#VS Codeの拡張機能をインストール
-# cat ./vscode-extensions.txt | while read line
-# do
-#     code --install-extension $line
-# done
-
-ln -s $(cd $(dirname $0); pwd)/settings.json $HOME/Library/ApplicationSupport/Code/User/settings.json
